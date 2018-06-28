e542116193749046b6b17213a1c34bed49d33d2f
athik13 <athik.13@gmail.com>
2018-06-28 10:15:54 +0500

    web

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,31 +6,32 @@
       <img src="/img/BG.jpg" style="width:100%; height:350px">
     
   {{-- </header>  --}}
-
-  <div class="card text-center" style="border-bottom:none;">
-    <div class="card-header">
-      <center>
-        <ul class="nav nav-pills card-header-pills">
-          <li class="nav-item">
-            <a class="nav-link active" href="#">Group Stage Fixtures</a>
-          </li>
-          {{-- <li class="nav-item">
-            <a class="nav-link" href="#">Knockout Stage Fixtures</a>
-          </li> --}}
-        </ul>
-      </center>
+  <section>
+    <div class="card text-center" style="border-bottom:none;">
+      <div class="card-header">
+        <center>
+          <ul class="nav nav-pills card-header-pills">
+            <li class="nav-item">
+              <a class="nav-link active" href="#">Group Stage Fixtures</a>
+            </li>
+            {{-- <li class="nav-item">
+              <a class="nav-link" href="#">Knockout Stage Fixtures</a>
+            </li> --}}
+          </ul>
+        </center>
+      </div>
+      <div class="card-body">
+          @foreach ($fixtures as $fixture)
+            @if ($fixture->match_end == 1)
+            <h6 class="card-title">FT</h6>
+            @endif
+            <h5 class="card-title">{{ $fixture->team1->name }} |  <span> {{ $fixture->score->team_one }} - {{ $fixture->score->team_two }} </span>  | {{ $fixture->team2->name }}</h5>
+            <p class="card-text">{{ $fixture->date }} | {{ $fixture->time_start }} - {{ $fixture->time_end }} | {{ $fixture->pitch->name }}</p>
+            <hr>
+          @endforeach
+      </div>
     </div>
-    <div class="card-body">
-        @foreach ($fixtures as $fixture)
-          @if ($fixture->match_end == 1)
-          <h6 class="card-title">FT</h6>
-          @endif
-          <h5 class="card-title">{{ $fixture->team1->name }} |  <span> {{ $fixture->score->team_one }} - {{ $fixture->score->team_two }} </span>  | {{ $fixture->team2->name }}</h5>
-          <p class="card-text">{{ $fixture->date }} | {{ $fixture->time_start }} - {{ $fixture->time_end }} | {{ $fixture->pitch->name }}</p>
-          <hr>
-        @endforeach
-    </div>
-  </div>
+  </section>
 
   <section id="standings">
     
