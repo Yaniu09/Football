a8c98e107f0fa570c83a9ff538526b0dfc3d5827
Yaniu09 <imperialaeiou@gmail.com>
2018-06-27 21:56:57 -0700

    srgh

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,46 +31,7 @@
   </div>
 
   <section id="standings">
-<<<<<<< HEAD
     
-=======
-    <div class="container">
-        <div class="container text-center">
-        <h1>Tournament Standings</h1>
-        </div>
-      <div class="row">
-        <div class="col-lg-8 mx-auto">
-          @foreach ($groups as $group)
-            <h3>{{ $group->name }}</h3>
-            <table class="table table-sm ">
-              <thead>
-                <tr>
-                  <th scope="col">Team</th>
-                  <th scope="col">MP</th>
-                  <th scope="col">GF</th>
-                  <th scope="col">GA</th>
-                  <th scope="col">GD</th>
-                  <th scope="col">Pts</th>
-                </tr>
-              </thead>
-              <tbody>
-                  @foreach ($group->teams as $team)
-                  <tr>
-                    <th scope="row" style="font-weight:normal;">{{ $team->name }}</th>
-                    <td>{{ $team->standing->mp }}</td>
-                    <td>{{ $team->standing->gf }}</td>
-                    <td>{{ $team->standing->ga }}</td>
-                    <td>{{ $team->standing->gd }}</td>
-                    <td>{{ $team->standing->pts }}</td>
-                  </tr>
-                  @endforeach
-              </tbody>
-            </table>
-          @endforeach
-        </div>
-      </div>
-    </div>
->>>>>>> cb7d765f5c02cacde8f6c3eaece9a2f3164b00a8
   </section>
 
 
