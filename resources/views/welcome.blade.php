e8ed742be1d936d493cc1f8ceb20249aceff42e2
Yaniu09 <imperialaeiou@gmail.com>
2018-06-29 07:07:15 -0700

    sdf

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,8 +15,16 @@
       <h2 class="title">About Us</h2>
       <p>Metro Fustsal Cup is an annual futsal tournament organized by Metro Fitness Group to promote unity amongst the youth. The tournament is a youth-led platform which aims to use futsal as a tool to promote the values of unity, reconciliation and encourage development among youth with the goal to lead a healthy lifestyle. Metro Fitness Group has also been proative in spreading messages of unity by encouraging youth to gain a better understanding of others and respect their peers.</p>
       <hr>
-      <h2 class="">Our Sponsor</h2>
+      <h3 class="">Main Sponsor</h2>
+      <center><img width="100%" src="/a.png"></center>
+      <br>
+      <hr>
+      <h3 class="">Other Sponsors</h2>
       <img src="/img/Sponsor.png" width="100%">
+      <br>
+      <center>
+      <img src="/b.png" width="75" height="75">
+      </center>
     </div>
   </section>
   
