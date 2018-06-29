4ffe0c8dd229b634d884dfa28c54b2d2028abc2e
Yaniu09 <imperialaeiou@gmail.com>
2018-06-28 23:09:44 -0700

    asda

---
+++ app/Http/Controllers/OtherfController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Otherf;
+use Illuminate\Http\Request;
+
+class OtherfController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Otherf  $otherf
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Otherf $otherf)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Otherf  $otherf
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Otherf $otherf)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Otherf  $otherf
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Otherf $otherf)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Otherf  $otherf
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Otherf $otherf)
+    {
+        //
+    }
+}
